e6a902cc5c693782f3fa99c720aacdacc49f036b
Kurt Madsen <kurt@crowdbotics.com>
2023-07-19 16:40:37 -0700

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,15 +1,14 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "test-kurt-03-41926",
-  url: "https://test-kurt-03-41926.botics.co",
-  api: "https://test-kurt-03-41926.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
-}
+  "name": "test-kurt-03-41926",
+  "url": "https://test-kurt-03-41926.botics.co",
+  "api": "https://test-kurt-03-41926.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "welcome"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
